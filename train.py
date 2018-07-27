3c92a541065ad446053b92acfe381e3e9650ab29
Franz <franz.weidmann@cs.hs-fulda.de>
2018-07-27 19:15:41 +0200

    fixed stats output during training

--- train.py
+++ train.py
@@ -5,14 +5,18 @@ import numpy as np
 
 def doTrain(epoch, sess, graph, config, data, modelFileName):
 
+    accuracySum = 0
     step = 1
 
-    loss = None
-    acc = None
+    loss = []
+    train_acc = []
+    acc = []
 
-    for batch in data.getNextBatchTrain(config["batchSize"]):
 
-        # this list contains corrupted images that create error which sets the loss to NaN
+    for batch in data.getNextBatchTrain(config["batchSize"], step*epoch):
+        
+
+        # images that create error which sets the loss to NaN
         faultyVals = list()
 
         if step not in faultyVals:
@@ -33,38 +37,37 @@ def doTrain(epoch, sess, graph, config, data, modelFileName):
             print("SKIPPED INVALID STEP: ", step)    
         
         if step % 100 == 0:
-            summary, _loss, train_acc = sess.run([graph["mergedLog"], graph["loss"], graph["accuracy"]], feed_dict=feed_dict)
+            summary, _loss, _train_acc = sess.run([graph["mergedLog"], graph["loss"], graph["accuracy"]], feed_dict=feed_dict)
             
-            if loss == None:
-                loss = _loss
-            else:
-                loss = (loss+_loss) / 2
+            
+            train_acc.append(_train_acc*100)
+            loss.append(_loss)
             
             graph["logWriter"].add_summary(summary, step)
 
-            train_acc *= 100
 
-            status = "Epoch : "+str(epoch)+" || Step: "+str(step+1)+"/"+str(int(int(data.config["size"]) * data.config["trainsize"]))
-            status += " || loss:"+str(round(loss, 3))+" || train_accuracy:"+ str(round(train_acc, 3))
+            status = "Epoch : "+str(epoch)+" || Step: "+str(step+1)+"/"+ str(data.config["trainSize"])
+            status += " || loss:"+str(round(np.mean(np.array(loss)), 3))+" || train_accuracy:"+ str(round(np.mean(np.array(train_acc)), 3))
             status += "% || time 1 step with batch of "+str(config["batchSize"])+": "+str(round(end-start, 3))
 
             print(status, end="\r")            
 
-        if step >= config["steps"]:
+        if step >= data.config["size"]:
             break
 
         step+=1
+    
 
-    # validate trained model after epoch
-    testData = data.getNextBatchTest(1)#data.getNextBatchTest(testBatch)
-    feed_dict={
-        graph["imagePlaceholder"]: np.expand_dims(testData[1], axis=3) if data.config["imageChannels"] == 1 else testData[1],
-        graph["labelPlaceholder"]: testData if data.config["imageChannels"] == 1 else testData[0]
-    }
-    
-    if acc == None:
-        acc = 100*(graph["accuracy"].eval(feed_dict=feed_dict))
-    else:
-        acc = (100*(graph["accuracy"].eval(feed_dict=feed_dict))+acc) / 2
-    
-    print("\ntest_accuracy: "+str(round(acc, 3)))
+    # validate trained model after one epoch
+    for validationData in data.getNextBatchValidation(config["batchSize"], data.config["validationSize"]):
+
+        feed_dict={
+            graph["imagePlaceholder"]: np.expand_dims(validationData[1], axis=3) if data.config["imageChannels"] == 1 else validationData[1],
+            graph["labelPlaceholder"]: validationData if data.config["imageChannels"] == 1 else validationData[0]
+        }
+
+        _acc = 100*(graph["accuracy"].eval(feed_dict=feed_dict))    
+        acc.append(_acc)
+
+    print("\nvalidation_accuracy: "+str(round(np.mean(np.array(acc)), 3)))
+    return accuracySum
